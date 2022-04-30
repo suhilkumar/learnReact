da4bed0bd6cb90412b57be46bf8e7120d7ed05ce
suhil <suhilkumarpatel@gmail.com>
2022-04-30 13:42:32 -0400

    create first jsx code and react-dom.render

---
+++ my-app/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+// this is jsx code means html inside js
+let element = <h1> Welcome to Sahosoft </h1> 
+
+ReactDom.render(
+    element, document.getElementById('root')
+);
