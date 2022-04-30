d15e19207157b191a547af92f368c29410a8e42a
suhil <suhilkumarpatel@gmail.com>
2022-04-30 14:07:46 -0400

    diff in jsx and react code

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,20 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-// this is jsx code means html inside js
-let element = <h1> Welcome to Sahosoft </h1> 
+import React, { createElement } from "react";
+import ReactDom from "react-dom";
 
-ReactDom.render(
-    element, document.getElementById('root')
-);
+// this is jsx code means html inside js
+// let element = <h1> Welcome to Sahosoft </h1>
+// ReactDom.render(
+//     element, document.getElementById('root')
+// );
+// ReactDom.render(element, document.getElementById("root"));
+
+
+// this is with the help of react code
+// in react createElement you pass 3 parameters 1 tag name , 
+// 2 is prop, 
+// 3 is content you can either store content in variable and use it
+// let element = React.createElement("h1", null, "suhil learning react");
+// let element1 = React.createElement("div", null, element);
+// ReactDom.render(element1, document.getElementById("root"));
+
+
