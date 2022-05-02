6d575f7ce5b8f530fa6405f639d46ab13608608e
suhil <suhilkumarpatel@gmail.com>
2022-05-02 16:36:02 -0400

    array.map method in JSX

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,11 +2,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const names = ["Suhil", "Rucha", "kirtan"]
+const names = ["Suhil", "Arpil","Rucha", "kirtan"]
 
-const nameList = [];
-for(const [index, value] of names.entries()){
-    nameList.push(<li key={index}>{value}</li>);
+// const nameList = [];
+// for(const [index, value] of names.entries()){
+//     nameList.push(<li key={index}>{value}</li>);
     
-}
-ReactDOM.render(<ul>{nameList}</ul>, document.getElementById("root"));
+// }
+// ReactDOM.render(<ul>{nameList}</ul>, document.getElementById("root"));
+ 
+// Now lets try in map method
+
+const nameList = <ul>
+    {names.map((v,i) => {
+        return <li key={i}> {v}</li>
+    })}
+</ul>
+
+ReactDOM.render(nameList, document.getElementById("root"))
