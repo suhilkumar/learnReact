82e3a4fee1a514b7896c2ac05a2f2f796b036ebf
suhil <suhilkumarpatel@gmail.com>
2022-05-02 18:02:42 -0400

    example of class component on single file

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,5 +1,38 @@
-import ReactDOM from 'react-dom';
-import {Student} from './app.js' //Named import
+import React, { Component } from 'react'
+import reactDom from 'react-dom'
 
+  class Header extends Component {
+  render() {
+    return (
+      <div>Header</div>
+    )
+  }
+}
+  class Footer extends Component {
+  render() {
+    return (
+      <div>Footer</div>
+    )
+  }
+}
 
-ReactDOM.render(<Student />, document.getElementById("root"))
+  class Content extends Component {
+  render() {
+    return (
+      <div>Suhil content </div>
+    )
+  }
+}
+
+  class Index extends Component {
+  render() {
+    return (<div>
+        <Header />
+        <Content />
+        <Footer />
+        
+        </div>)
+  }
+}
+
+reactDom.render(<Index />, document.getElementById("root"))
