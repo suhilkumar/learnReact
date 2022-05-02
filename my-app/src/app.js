88be2b635749e0da7b9e2b3f7b5381852532de4e
suhil <suhilkumarpatel@gmail.com>
2022-05-02 17:54:20 -0400

    import class Component from diff file

---
+++ my-app/src/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Student extends Component {
+    render() {
+        return <h2> Class Component</h2>
+    }
+}
+// default export
+// export default Student;
+
+// Named export
+export {Student}
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,5 @@
-import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import {Student} from './app.js' //Named import
 
-class Student extends Component {
-    render() {
-        return <h2> Class Component</h2>
-    }
-}
 
 ReactDOM.render(<Student />, document.getElementById("root"))
