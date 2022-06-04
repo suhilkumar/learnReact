317ba99721b8fa7eee93ef8aa73b62edc387ea55
suhil <suhilkumarpatel@gmail.com>
2022-06-04 13:07:32 -0400

    use boolean in props

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-function Employee (props){
+function Employee(props) {
   return (
-    <div>Hi {props.name}, my age is {props.age}</div>
-  )
+    // boolean value doesn't show in react you have to use condition operator or ternary operator
+    <div>
+      Hi {props.name}, my age is {props.age}
+      <h3> Your gender: {props.gender === true ? "Male": "Female"}</h3>
+      <h4>do you have a child : {props.isChild === true ? "yes":"no"}</h4>
+    </div>
+  );
 }
 // note : for any data types other then string you must use curly brackets
-ReactDOM.render(<Employee name="Suhil" age={28}/>, document.getElementById("root"))
+ReactDOM.render(
+  <Employee name="Suhil"
+   age={28} 
+   gender = {true}
+   isChild = {false}/>,
+  document.getElementById("root")
+);
