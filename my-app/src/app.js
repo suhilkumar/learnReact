507486a381140344c68a38e591d4890a4f7c2de6
suhil <suhilkumarpatel@gmail.com>
2022-06-04 12:15:19 -0400

    use constructor in class component

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -1,20 +1,44 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
-export class Header extends Component {
+// export class Header extends Component {
+//   render() {
+//     return <div>Header</div>;
+//   }
+// }
+// export class Footer extends Component {
+//   render() {
+//     return <div>Footer</div>;
+//   }
+// }
+
+// export class Content extends Component {
+//   render() {
+//     return <div>Suhil content </div>;
+//   }
+// }
+
+// // Note: if you dont want to write named export on bottom you can write export on class 
+
+import React, { Component } from 'react';
+
+class Student extends React.Component {
+  constructor(){
+    super()
+    // this.objSub = {
+    //   subject: "Math"
+    // }
+// this is not a recommended approach you must always use state 
+
+this.state = {
+  subject:"math",
+  age: 28,
+  name : "Suhilkumar"
+}
+  }
+
   render() {
-    return <div>Header</div>;
+    return <h2>I am {this.state.name}, my age is {this.state.age} and I am Poor Student in {this.state.subject}</h2>;
   }
 }
-export class Footer extends Component {
-  render() {
-    return <div>Footer</div>;
-  }
-}
 
-export class Content extends Component {
-  render() {
-    return <div>Suhil content </div>;
-  }
-}
-
-// Note: if you dont want to write named export on bottom you can write export on class 
+export {Student}
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,18 +1,41 @@
-import React, { Component } from 'react'
-import reactDom from 'react-dom'
-// if you want to import multiple file you can import in curly brackets with the help of ,
-import {Header, Content, Footer} from './app'
-  
+// import React, { Component } from 'react'
+// import reactDom from 'react-dom'
+// // if you want to import multiple file you can import in curly brackets with the help of ,
+// import {Header, Content, Footer} from './app'
 
-  class Index extends Component {
-  render() {
-    return (<div>
-        <Header />
-        <Content />
-        <Footer />
-        
-        </div>)
-  }
-}
+//   class Index extends Component {
+//   render() {
+//     return (<div>
+//         <Header />
+//         <Content />
+//         <Footer />
 
-reactDom.render(<Index />, document.getElementById("root"))
+//         </div>)
+//   }
+// }
+
+// reactDom.render(<Index />, document.getElementById("root"))
+
+import ReactDOM from "react-dom";
+import {Student} from "./app"
+
+// class Student extends React.Component {
+//   constructor(){
+//     super()
+//     // this.objSub = {
+//     //   subject: "Math"
+//     // }
+// // this is not a recommended approach you must always use state 
+
+// this.state = {
+//   subject:"math",
+//   age: 28,
+//   name : "Suhilkumar"
+// }
+//   }
+
+//   render() {
+//     return <h2>I am {this.state.name}, my age is {this.state.age} and I am Poor Student in {this.state.subject}</h2>;
+//   }
+// }
+ReactDOM.render(<Student />, document.getElementById("root"));
