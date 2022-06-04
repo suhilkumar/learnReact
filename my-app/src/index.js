6f0570905071733eb0e9bbc8bb0c630d17eabf55
suhil <suhilkumarpatel@gmail.com>
2022-06-04 12:52:19 -0400

    destructuring in props

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -24,9 +24,43 @@ import ReactDOM from 'react-dom';
 // now we use props and pass arguments with props
 // You declare the values in parent component and pass in child components as props 
 
+// function Greeting (props){
+//   return(
+//     <p>Hi {props.name}, my age is {props.age}</p>
+//   )
+// }
+// function Index(){
+//   return(
+//     <>
+//     <Greeting name="Suhil" age="32" />
+//     <Greeting name="Rucha" age="27" />
+//     </>
+//   )
+// }
+// ReactDOM.render(<Index/>, document.getElementById("root"))
+
+// // Destructuring 1 way just write properties in curly brackets
+// function Greeting ({name,age}){
+//   return(
+//     <p>Hi {name}, my age is {age}</p>
+//   )
+// }
+// function Index(){
+//   return(
+//     <>
+//     <Greeting name="Suhil" age="32" />
+//     <Greeting name="Rucha" age="27" />
+//     </>
+//   )
+// }
+// ReactDOM.render(<Index/>, document.getElementById("root"))
+
+
+// destructuring 2 way
 function Greeting (props){
+  const {name, age} = props;
   return(
-    <p>Hi {props.name}, my age is {props.age}</p>
+    <p>Hi {name}, my age is {age}</p>
   )
 }
 function Index(){
