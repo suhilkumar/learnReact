b2f80c9e5eae36e000232b6320a5782d32e57a8c
suhil <suhilkumarpatel@gmail.com>
2022-06-04 13:03:07 -0400

    string and num data type in props

--- my-app/src/app.js
+++
@@ -1,6 +0,0 @@
-export default function Greeting (props){
-  const {name, age} = props;
-  return(
-    <p>Hi {name}, my age is {age}</p>
-  )
-}
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,69 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// // create a functional component
-// function Greeting(){
-//   return (
-//     <p>Hi Suhil,</p>
-//   )
-// }
 
-// // create another component and pass greeting component inside this component
-// function Index() {
-//   return (
-//     <div>
-//       <Greeting />
-//     </div>
-//   )
-// }
-
-// // render this component
-// ReactDOM.render(<Index/>, document.getElementById("root"))
-
-// above was not a recommended approach now we use props 
-
-// now we use props and pass arguments with props
-// You declare the values in parent component and pass in child components as props 
-
-// function Greeting (props){
-//   return(
-//     <p>Hi {props.name}, my age is {props.age}</p>
-//   )
-// }
-// function Index(){
-//   return(
-//     <>
-//     <Greeting name="Suhil" age="32" />
-//     <Greeting name="Rucha" age="27" />
-//     </>
-//   )
-// }
-// ReactDOM.render(<Index/>, document.getElementById("root"))
-
-// // Destructuring 1 way just write properties in curly brackets
-// function Greeting ({name,age}){
-//   return(
-//     <p>Hi {name}, my age is {age}</p>
-//   )
-// }
-// function Index(){
-//   return(
-//     <>
-//     <Greeting name="Suhil" age="32" />
-//     <Greeting name="Rucha" age="27" />
-//     </>
-//   )
-// }
-// ReactDOM.render(<Index/>, document.getElementById("root"))
-
-
-// destructuring 2 way
-import Greeting from './app';
-function Index(){
-  return(
-    <>
-    <Greeting name="Suhil" age="32" />
-    <Greeting name="Rucha" age="27" />
-    </>
+function Employee (props){
+  return (
+    <div>Hi {props.name}, my age is {props.age}</div>
   )
 }
-ReactDOM.render(<Index/>, document.getElementById("root"))
+// note : for any data types other then string you must use curly brackets
+ReactDOM.render(<Employee name="Suhil" age={28}/>, document.getElementById("root"))
