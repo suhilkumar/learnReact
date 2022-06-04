46322cf27a6c5440869b826022a6f3d44903fce3
suhil <suhilkumarpatel@gmail.com>
2022-06-04 13:26:50 -0400

    use array and object in props

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,21 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+let friends = ["suhil", "rucha", "kirtan"];
+let studentInfo = { mobile: 9125969819, email: "suhil.patel009@gmail.com" };
 function Employee(props) {
   return (
-    // boolean value doesn't show in react you have to use condition operator or ternary operator
     <div>
-      Hi {props.name}, my age is {props.age}
-      <h3> Your gender: {props.gender === true ? "Male": "Female"}</h3>
-      <h4>do you have a child : {props.isChild === true ? "yes":"no"}</h4>
+      <h2>your name is {props.name}</h2>
+      <h3> your age is {props.age}</h3>
+      <h4> your friend :
+          <ul>
+            {
+            props.friend.map(function namePrint(item, i){
+              return <li  key={i}>{item}</li>
+            })
+            }
+          </ul>
+      </h4>
+      <h3> your info : {props.info.mobile}</h3>
+      <h3> your info : {props.info.email}</h3>
     </div>
   );
 }
-// note : for any data types other then string you must use curly brackets
+
 ReactDOM.render(
-  <Employee name="Suhil"
-   age={28} 
-   gender = {true}
-   isChild = {false}/>,
+  <Employee name="Patel" age={32}
+  friend= {friends} info={studentInfo}/>,
   document.getElementById("root")
 );
