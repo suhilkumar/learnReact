67df1dba9e99fafaa6136152f31dd992964c46c6
suhil <suhilkumarpatel@gmail.com>
2022-06-11 10:12:46 -0400

    define the data type in proptype

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,5 +1,37 @@
-import ReactDOM from 'react-dom'
-import Student from './app'
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<Student name="Suhilkumar">hi whats up suhil</Student>, document.getElementById("root"))
-// hi whats up suhil => they are children
+// If you want to apply checkers in props you use propstype
+// SAMPLE FUNCTION COMPONENT
+// function Sidebar(props) {
+//   return <div> {props.children}</div>;
+// }
+
+// ReactDOM.render(
+//   <Sidebar>
+//     <nav>
+//       <a href='#'> Home </a>
+//       <a href='#'> About </a>
+//       <a href='#'> Content</a>
+//     </nav>
+//   </Sidebar>,
+//   document.getElementById("root")
+// );
+
+// PROPS TYPE
+
+import propTypes from "prop-types";
+
+Emp.propTypes = {
+    name: propTypes.string,
+}
+function Emp(props) {
+  return (
+    <div>
+      <h2>Hello, {props.name}</h2>
+      <h3>age: {props.age}</h3>
+    </div>
+  );
+}
+
+ReactDOM.render(<Emp name="Suhil" age={29}/>, document.getElementById("root"));
