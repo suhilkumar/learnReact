0fc2543a77e7d83060bc88eda22f512b44a38a3c
suhil <suhilkumarpatel@gmail.com>
2022-06-11 19:52:48 -0400

    parent state in child component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,56 +1,46 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-// syntax
-// eventName = {functionName}
+class Second extends Component {
+  render() {
+    return <div> <h1>Child Component</h1> Counter =  {this.props.counter}</div>;
+  }
+}
 
-// lets take a example of onclick event showing a alert message in function component
+export default class Index extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
 
-// function showMsg(){
-//     alert("Hi Rucha learning events in react")
-// }
+  handleDecrement = () => {
+    this.setState((state) => ({
+      count: state.count - 1,
+    }));
+  };
 
-// function Index (){
-//     return(
-//         <>
-//         <button type="button" onClick={showMsg}>press</button>
-//         </>
-//     )
-// }
+  handleIncrement = () => {
+    this.setState((state) => ({
+      count: state.count + 1,
+    }));
+  };
+  render() {
+    return (
+      <div>
+        <Second counter={this.state.count} />
+        <h1>Parent Component</h1>
+        
+        <button type="button" onClick={this.handleIncrement}>
+          increment by 1
+        </button>
+        <button type="button" onClick={this.handleDecrement}>
+          Decrement by 1
+        </button>
+      </div>
+    );
+  }
+}
 
-// ReactDOM.render(<Index/>,document.getElementById("root"))
-
-// class based component in react but notice 1 diff we use this keyword for function
-// class Index extends Component {
-//   showMsg() {
-//     alert("Hi Rucha learning events in react");
-//   }
-
-//   render() {
-//     return (
-//       <>
-//         <button type="button" onClick={this.showMsg}>
-//           press
-//         </button>
-//       </>
-//     );
-//   }
-// }
-// ReactDOM.render(<Index />, document.getElementById("root"));
-
-// Example of creating a url and alert message
-// function showMsg(e){
-//     // sythentic events arr used for stopping the url 
-//     e.preventDefault();
-//     alert("Hi Rucha learning events in react")
-// }
-
-// function Index (){
-//     return(
-//         <>
-//         <a href="google.com" onClick={showMsg}>click</a>
-//         </>
-//     )
-// }
-
-// ReactDOM.render(<Index/>,document.getElementById("root"))
+ReactDOM.render(<Index></Index>, document.getElementById("root"));
