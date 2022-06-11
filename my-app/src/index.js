046491172d14671c82b01050611da0bf27dbbb34
suhil <suhilkumarpatel@gmail.com>
2022-06-11 12:06:17 -0400

    prop validation in prop types

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// import React, { Component } from "react";
+// import ReactDOM from "react-dom";
 
 // If you want to apply checkers in props you use propstype
 // SAMPLE FUNCTION COMPONENT
@@ -20,18 +20,35 @@ import ReactDOM from "react-dom";
 
 // PROPS TYPE
 
-import propTypes from "prop-types";
+// import propTypes from "prop-types";
 
-Emp.propTypes = {
-    name: propTypes.string,
-}
-function Emp(props) {
-  return (
-    <div>
-      <h2>Hello, {props.name}</h2>
-      <h3>age: {props.age}</h3>
-    </div>
-  );
-}
+// Emp.propTypes = {
+//     name: propTypes.string,
+// }
+// function Emp(props) {
+//   return (
+//     <div>
+//       <h2>Hello, {props.name}</h2>
+//       <h3>age: {props.age}</h3>
+//     </div>
+//   );
+// }
 
-ReactDOM.render(<Emp name="Suhil" age={29}/>, document.getElementById("root"));
+// ReactDOM.render(<Emp name="Suhil" age={29}/>, document.getElementById("root"));
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types'
+
+class Emp extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello, {this.props.name}</h1>
+            </div>
+        )
+    }
+}
+Emp.propType = {
+    name : PropTypes.string.isRequired
+}
+ReactDOM.render(<Emp />, document.getElementById("root"))
