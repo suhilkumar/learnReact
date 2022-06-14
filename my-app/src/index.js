2260475bc2282b920e10a4b2bbaa86772b51e9c7
suhil <suhilkumarpatel@gmail.com>
2022-06-14 19:59:14 -0400

    add material ui in react

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom';
-import './index.css'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Button from '@mui/material/Button';
 
 export default class Index extends Component {
-
   render() {
     return (
       <div>
         <h1>Hi Suhil</h1>
         <h2>use scss</h2>
         <h3>instead of css</h3>
-        <button type='button'>Save</button>
-        <button className='btn btn-success'>Save</button>
-        <button className='btn btn-danger'>Save</button>
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
       </div>
-    )
+    );
   }
 }
-ReactDOM.render(<Index />, document.getElementById("root"))
+ReactDOM.render(<Index />, document.getElementById("root"));
