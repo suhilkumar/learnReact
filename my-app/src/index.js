5fa38b5944cddcb2367b65cd6b247134a8de506e
suhil <suhilkumarpatel@gmail.com>
2022-06-14 19:38:21 -0400

    added google fonts in react

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
-import './index.scss'
+import './index.css'
 export default class Index extends Component {
   render() {
     return (
