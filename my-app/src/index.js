4709e5dc0d71c8dd775e14dc1d5719685d271e61
suhil <suhilkumarpatel@gmail.com>
2022-06-14 19:52:39 -0400

    add bootstrap to react

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
 import './index.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
 export default class Index extends Component {
+
   render() {
     return (
       <div>
         <h1>Hi Suhil</h1>
         <h2>use scss</h2>
         <h3>instead of css</h3>
+        <button type='button'>Save</button>
+        <button className='btn btn-success'>Save</button>
+        <button className='btn btn-danger'>Save</button>
       </div>
     )
   }
