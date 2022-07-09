823cae85faa981432f29a338be2af9488b8a05a6
suhil <suhilkumarpatel@gmail.com>
2022-07-09 15:14:59 -0500

    add navigation for filtering data

--- src/component/Basics/resturant.jsx
+++ src/component/Basics/resturant.jsx
@@ -1,18 +1,33 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './style.css'
 import Menu from "./menuApi"
 import MenuCard from './MenuCard'
 
 
 export const Resturant = () => {
-    const [menuData, setMenuData]=useState(Menu)
-    
-    return(
+    const [menuData, setMenuData] = useState(Menu)
+    const filterItem = (category) => {
+        const updatedList = Menu.filter((curElem) => {
+            return curElem.category === category
+        })
+        setMenuData(updatedList)
+    }
+
+    return (
         <div>
-            
-            <MenuCard menuData={menuData}/>
+            <nav className='navbar'>
+                <div className='btn-group'>
+                    <button className='btn-group__item' onClick={() => filterItem("breakfast")}>Breakfast</button>
+                    <button className='btn-group__item' onClick={() => filterItem("lunch")}>Lunch</button>
+                    <button className='btn-group__item' onClick={() => filterItem("evening")}>Evening</button>
+                    <button className='btn-group__item' onClick={() => filterItem("dinner")}>Dinner</button>
+                    <button className='btn-group__item' onClick={() => setMenuData(Menu)}>All</button>
+                </div>
+
+            </nav>
+            <MenuCard menuData={menuData} />
         </div>
     )
- 
+
 }
 
